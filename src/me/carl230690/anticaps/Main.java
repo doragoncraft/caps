ca504029e7e278a12c49667a80a45c9549d507ac
Nick <Nick>
2013-09-17 17:04:37 -0400

    removed unused imports

--- src/me/carl230690/anticaps/Main.java
+++ src/me/carl230690/anticaps/Main.java
@@ -1,12 +1,9 @@
 package me.carl230690.anticaps;
 
-import org.bukkit.Server;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
